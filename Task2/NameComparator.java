33326224c49982e9330c47fcbe002581c71f2295
Rocketman887 <69236669+Rocketman887@users.noreply.github.com>
2020-09-03 18:34:44 +0300

    Delete NameComparator.java

--- Task2/NameComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class NameComparator implements Comparator<Student> {
-    //Здесь сортировал по имени по алфавиту
-    @Override
-    public int compare(Student o1, Student o2) {
-        return o1.getName().compareTo(o2.getName());
-    }
-}
